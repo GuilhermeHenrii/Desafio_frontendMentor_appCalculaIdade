dc348ebc98a44a15c6dbb0d12b3d948d5536afd1
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-05-29 16:52:40 -0300

    tratando inputs

--- script/index.js
+++ script/index.js
@@ -16,15 +16,16 @@ class Age {
         const month = this.form.querySelector('#month');
         const year = this.form.querySelector('#year');
         this.acceptedDate = new Date(`${year.value}-${month.value}-${day.value}T00:00:00-03:00`);
-        console.log(this.acceptedDate);
 
         const fieldValid = this.fieldIsValid();
         const dateValid = this.dateBrIsValid();
         const calculateAge = this.calculateAge(this.acceptedDate);
 
-        // if(fieldValid && dateValid){
-        //     alert('data e campos validos');
-        // }
+
+        console.log(calculateAge);
+        // fieldValid == true ? alert('campos ok'): alert('campos nao ok');
+        // dateValid === true ? alert('datas ok'): alert('campos de data nao ok');
+        // calculateAge === true ? alert('calculo feito') : alert('calculo nao feito');
     };
 
     fieldIsValid() {
@@ -37,6 +38,30 @@ class Age {
         for (let field of this.form.querySelectorAll('.input')) {
             const label = field.previousElementSibling.innerText;
 
+            if(field.getAttribute('id') === 'day'){
+                const valueOfday = this.form.querySelector('#day');
+                if(valueOfday.value.length !== 2){
+                    valid = false;
+                    this.createError(field, `O campo ${label} deve conter 2 caracteres`);
+                }
+            }
+
+            if(field.getAttribute('id') === 'month'){
+                const valueOfMonth = this.form.querySelector('#month');
+                if(valueOfMonth.value.length !== 2){
+                    valid = false;
+                    this.createError(field, `O campo ${label} deve conter 2 caracteres`);
+                }
+            }
+
+            if(field.getAttribute('id') === 'year'){
+                const valueOfday = this.form.querySelector('#year');
+                if(valueOfday.value.length !== 4){
+                    valid = false;
+                    this.createError(field, `O campo ${label} deve conter 4 caracteres`);
+                }
+            }
+            
             if (!field.value) {
                 this.createError(field, `Field ${label.toLowerCase()} empty`);
                 valid = false;
@@ -47,10 +72,12 @@ class Age {
                 valid = false;
             }
         }
+
         return valid;
     };
 
     dateBrIsValid() {
+        let valid = true;
         const dateBr = this.acceptedDate.toLocaleDateString('pt-br', {
             year: "numeric",
             month: "2-digit",
@@ -58,51 +85,52 @@ class Age {
             timeZone: 'America/Sao_Paulo'
         });
 
-        console.log(dateBr);
-
         let regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
         const currentDate = new Date().getFullYear();
         if (!regex.test(dateBr)) {
-            return console.log('data não corresponde ao formato dd-mm-aaaa'); // não corresponde ao formato dd-mm-aaaa
+            valid = false;
+            return console.log('data não corresponde ao formato dd-mm-aaaa');// não corresponde ao formato dd-mm-aaaa
         };
 
         let dateParts = dateBr.split("-");
         let day = parseInt(dateParts[0]);
         let month = parseInt(dateParts[1]);
         let year = parseInt(dateParts[2]);
-        console.log(dateParts);
 
-        if (day < 0 || day > currentDate) {
+        if (year < 0 || year > currentDate) {
+            valid = false;
             return console.log(false); // ano deve estar entre 00 e 99
         };
         if (month < 1 || month > 12) {
+            valid = false;
             return false; // mês deve estar entre 01 e 12
         };
         if (day < 1 || day > 31) {
+            valid = false;
             return false; // dia deve estar entre 01 e 31
         };
         if ((month === 4 || month === 6 || month === 9 || month === 11) && day > 30) {
+            valid = false;
             return false; // meses com 30 dias
         };
         if (month === 2) {
             let leapYear = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
             if (leapYear && day > 29 || !leapYear && day > 28) {
+                valid = false;
                 return false; // fevereiro com 28 ou 29 dias
             };
         };
-        console.log(dateBr);
-        // const dtBrForEua = new Date(`${this.year.value}-${this.month.value}-${this.day.value}T00:00:00-03:00`);
-        return this.acceptedDate;
+        return valid;
     };
 
     calculateAge(dateOfBirth) {
+        let valid = true;
         dateOfBirth.toLocaleDateString('pt-br', {
             day: "2-digit",
             month: "2-digit",
             year: "numeric",
             timeZone: 'America/Sao_Paulo'
-        })
-        console.log(dateOfBirth instanceof Date);
+        });
         const ageDiffMs = Date.now() - dateOfBirth.getTime();
         const ageDate = new Date(ageDiffMs);
         const age = {
@@ -113,9 +141,8 @@ class Age {
             days: Math.floor(((ageDate % (30.44 * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000)))
 
         }
-        console.log(ageDiffMs);
         const ageArr = Object.values(age);
-        console.log(ageArr)
+        console.log(ageArr);
         return ageArr;
     };
 
